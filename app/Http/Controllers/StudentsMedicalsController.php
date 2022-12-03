a575e8b6adc10f763fed2a90ec7adc022092b518
badchita <47920377+badchita@users.noreply.github.com>
2022-12-04 03:02:27 +0800

    udpate file path saving

--- app/Http/Controllers/StudentsMedicalsController.php
+++ app/Http/Controllers/StudentsMedicalsController.php
@@ -61,7 +61,7 @@ class StudentsMedicalsController extends Controller
         $medicals = StudentsMedicals::where('created_at', '>' , $startOfYear)->where('created_at', '<', $endOfYear)->get();
         $count = count($medicals) + 1;
         $fileName = $count.'_'.$request->file->getClientOriginalName();
-        $request->file('file')->move('uploads/medicals', $fileName, 'public');
+        $request->file('file')->move(public_path() . 'uploads/medicals', $fileName, 'public');
         $this->fileName = $fileName;
 
         $response = [
