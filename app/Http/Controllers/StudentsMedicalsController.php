dc6b41dea14384ba8d05353d01e51ccbad691910
badchita <47920377+badchita@users.noreply.github.com>
2022-12-05 02:05:21 +0800

    update storeAs

--- app/Http/Controllers/StudentsMedicalsController.php
+++ app/Http/Controllers/StudentsMedicalsController.php
@@ -65,8 +65,8 @@ class StudentsMedicalsController extends Controller
         $name = $request->file->getClientOriginalName();
         $name = str_replace(' ', '_', $name);
         $fileName = $count.'_'.$name;
-        // $request->file('file')->store('/uploads/medicals' . $fileName, 'public');
-        $request->file('file')->move(public_path('storage/uploads/medicals'), $fileName);
+        $request->file('file')->store('/uploads/medicals', $fileName);
+        // $request->file('file')->move(public_path('storage/uploads/medicals'), $fileName);
         // $request->file('file')->storeAs('uploads/medicals', $fileName);
         $this->fileName = $fileName;
 
